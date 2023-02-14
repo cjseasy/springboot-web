9c52f37222ae98d52d5ac57ce1bb8b9de22d3e9a
建树的 <1569619255@qq.com>
2023-02-14 18:44:10 +0800

    测试提交

--- src/main/java/com/jianshu/SpringbootWebApplication.java
+++ src/main/java/com/jianshu/SpringbootWebApplication.java
@@ -1,9 +1,11 @@
 package com.jianshu;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@AutoConfiguration
 public class SpringbootWebApplication {
 
     public static void main(String[] args) {
